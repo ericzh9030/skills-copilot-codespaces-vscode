dd1d4eae05a97eff297ceca310ae95fc7e33278b
ericzh9030 <141296843+ericzh9030@users.noreply.github.com>
2023-08-03 04:54:02 +0000

    add isPrime()

--- skills.js
+++ skills.js
@@ -1,3 +1,10 @@
 function calculatesNumbers(var1, var2) {
     return var1 + var2
 }
+
+function isPrime(number) {
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) return false
+    }
+    return number > 1
+}
